2eafdbfb7a0dfe3bb44e63708edefac31017eba1
istekhar44 <67890istekharalam@gmail.com>
2025-03-29 11:05:17 -0700

    variables in the javaScript

---
+++ data-type-varivles/index.js
@@ -0,0 +1,43 @@
+//    printing the counting usig the do while loop
+let i=1;
+do{
+    console.log(i);
+    i++;
+}while(i<10){
+    console.log(i);
+}
+
+//    printing the counting using the for loop 
+for(let i=1; i<=10 ; i++){
+ console.log(i);
+}
+
+    //    calculator making 
+let num1 = Number(prompt("Enter the integer"));
+console.log(Number ,"Enter the number");
+
+let operator = operator(prompt("enter the operator (+ ,- ,* ,% ,/"))
+
+let num2 = Number(prompt("Enter the integer"));
+console.log(Number ,"Enter the number");
+
+if(operator === "+"){
+    console.log('result {num1 + num2}');
+    
+}else if (operator = "-"){
+    console.log('result {num1-num2}');
+}else if (operator = "*"){
+    console.log('result {num * num2}');
+}
+else {
+    console.log('Invalid operator');
+}
+        // varible for dynamically 
+let marks = 20;
+marks= "baaar";
+marks = true;
+    // assign the large number and the udefine error
+let marks = 8598094858459840584858448355885845845884095348584095948509409485845984598;
+console.log(marks);
+marks ;
+console.log(marks);
