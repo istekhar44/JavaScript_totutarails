9b96badf2a474cf67971d502f1ce59c773b97f99
istekhar44 <67890istekharalam@gmail.com>
2025-03-30 09:10:18 -0700

    add the java function

--- operator_Conditions/index.js
+++ operator_Conditions/index.js
@@ -1,5 +1,21 @@
-// let a= 4;
-// console.log(a);
+
+ //print the num      
+let c= 4;
+console.log(c);
+    //    Comparision between the two values
 let a=12;
 let b=13;
 console.log(a>=b);
+
+            //    condition statemant
+let age = 2;
+ let status1 = (age > 18) ? "i can vote" : " I can't vote";
+ console.log(status1);
+   
+        //  And operator
+ let ans= (true && true && false )
+ console.log(ans);
+
+        //  use the none boolean 
+let hellow= ( true && 'babbar' );
+console.log(hellow);
