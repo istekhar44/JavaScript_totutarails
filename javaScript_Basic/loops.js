a9322674fec9e5d8a87b61ff1b5b9739a125fe35
istekhar44 <67890istekharalam@gmail.com>
2025-03-31 08:58:01 -0700

    loop

---
+++ javaScript_Basic/loops.js
@@ -0,0 +1,38 @@
+// print the love babbar 10 times 
+
+// let ballu='love babbar';
+// for(let i = 1 ; i < 10 ; i++){
+//     console.log(ballu);
+// }
+
+// print the hellow ten times using the do while 
+// let hellow = 'print the loop';
+// let i=0;
+// do{
+//     console.log(hellow);
+//     i++;
+// }while(i<10);
+//     console.log(hellow)
+
+// printing the counting 
+
+// for(let i= 1; i<10 ; i++){
+//     console.log(i);
+// }
+
+//print the reverse counting
+// for(let i =5; i >0; i--){
+//     console.log(i);
+// }
+
+// printing the pattern type question
+let num=5;
+let pattern =' ';
+for(let i = 1 ; i<=num ; i++){
+    for(let j = 1 ; j <= i ; j++){
+        pattern += '*';
+    }
+    pattern += '\n';
+}
+console.log(pattern);
+
