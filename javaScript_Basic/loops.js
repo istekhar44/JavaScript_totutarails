51c433df459efe37c7cb96673870e0c294c7b617
istekhar44 <67890istekharalam@gmail.com>
2025-04-03 08:49:03 -0700

    add the info

--- javaScript_Basic/loops.js
+++ javaScript_Basic/loops.js
@@ -1,31 +1,31 @@
 // print the love babbar 10 times 
 
-// let ballu='love babbar';
-// for(let i = 1 ; i < 10 ; i++){
-//     console.log(ballu);
-// }
+let ballu='love babbar';
+for(let i = 1 ; i < 10 ; i++){
+    console.log(ballu);
+}
 
 // print the hellow ten times using the do while 
-// let hellow = 'print the loop';
-// let i=0;
-// do{
-//     console.log(hellow);
-//     i++;
-// }while(i<10);
-//     console.log(hellow)
+let hellow = 'print the loop';
+let i=0;
+do{
+    console.log(hellow);
+    i++;
+}while(i<10);
+    console.log(hellow)
 
 // printing the counting 
 
-// for(let i= 1; i<10 ; i++){
-//     console.log(i);
-// }
+for(let i= 1; i<10 ; i++){
+    console.log(i);
+}
 
 //print the reverse counting
-// for(let i =5; i >0; i--){
-//     console.log(i);
-// }
+for(let i =5; i >0; i--){
+    console.log(i);
+}
 
-// printing the pattern type question
+// printing the pattern type question (triangel pattern)
 let num=5;
 let pattern =' ';
 for(let i = 1 ; i<=num ; i++){
@@ -35,4 +35,23 @@ for(let i = 1 ; i<=num ; i++){
     pattern += '\n';
 }
 console.log(pattern);
+
+//2nd pattern for the (left triangle)
+
+
+
+
+
+
+
+//use of continue in the loop  (which skip the itration)
+for(let i = 1; i<=4 ;i++){
+    if (i==3){
+        continue;
+    }else{
+        console.log(i);
+    }
+}
+
+//
 
