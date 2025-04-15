b86b96c5930d5cda446527769ddf37c3b7846d68
istekhar44 <67890istekharalam@gmail.com>
2025-04-15 08:21:56 -0700

    add the string file

--- javaScript_Basic/varible.js
+++ javaScript_Basic/varible.js
@@ -7,3 +7,30 @@
 // console.log(sum);
 
 
+// let student = {
+//     name : "rahul",
+//     age : 20 ,
+//     cgpa :7.4,
+//     isPass: true
+
+
+// };
+// for(let key in student ){
+//     console.log("key=" ,key, "value=", student [key]);
+// }
+
+// for (let num = 0 ; i<= 100; num++){
+//     if (num % 2 !== 0){
+//         console.log("num = " num);
+        
+//     }
+// }
+
+
+let gameNum= 25;
+let userNum = prompt("gues the no ");
+while (userNum !== gameNum){
+ userNum  = prompt= ("You enter the roung no:");
+
+}
+console.log("Congratulatiom, You gess the correct no");
