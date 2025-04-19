1f5ef22786451f1add0840e1a6517b1c4d917f6f
istekhar44 <67890istekharalam@gmail.com>
2025-04-19 08:53:34 -0700

    Refactor JavaScript files: add new function implementations, remove unused files, and enhance code structure

--- Call_Stack_&_Hosting.js
+++ Call_Stack_&_Hosting.js
@@ -1,9 +1,93 @@
+// In JavaScript, hoisting is a behavior where variable and function declarations are moved (or "hoisted") to the top of their scope during the compilation phase, before code execution.
+
+// There are two main types of hoisting:
+// var age = 25;
+// console.log(age);
+
 // let num = 3;
 // switch (num){
 //     case 1 : console.log ("a");
 //     break;
 //     case 2 : console.log ("b");
 //     break;
+//     case 3 : console.log("hellow");
+//     break;
 // }
+
+// let a = 25;
+// console.log(a);
+
+// function sayHellow(){
+//     console.log("hellloow jee ")
+// }
+
+
+// function funD(){
+//     console.log("This is function ");
+
+// }
+// function funb(){
+//     console.log("Stackk 2");
+
+//     function fun
+// }
+
+// function greetME(greet, fullname ){
+//     console.log("hell0w " fullname);
+//     greet();
+
+
+// }
+// function greet(){
+//     console.log ("greet for the day")
+// }
+// greetME(greet ,"Babbar");
+// Create the function in the arry 
+// const arr = [
+//     function (a,b){
+//         return a+b;
+//     },
+//     function (a,b){
+//         return a*b;
+
+//     },
+//     function (a,b){
+//         return a/b;
+//     }
+// ];
+// let first = arr[1];
+// let ans = first (3,5);
+// console.log(ans);
+
+
+//function in the class and the object 
+// let object = {
+//     name : "babbar",
+//     class : "Btech",
+//     rollNO : 5,
+//     greet : ()=>{
+//         console.log("hellow jee");
+//     },
+//     hellow(a,b){
+//         return a+b;
+
+//         console.log("hellow jee kaso ho sara ");
+//     }
+// };
+// //printing the element from the object
+// console.log(object.name);
+// //calling the finction from the object
+// object.greet();
+
+
+
+
+//Question making the array and call the function from the arry 
+let arr =[ function(a,b){
+    
+}]
+
+
+
 
 
