133247c5e7cea47b35caa23630235da2a46b58f1
istekhar44 <67890istekharalam@gmail.com>
2025-05-01 06:46:18 -0700

    add the object file

--- Class_Obj.js
+++ Class_Obj.js
@@ -1,90 +1,110 @@
-// // Defining the class
-// class Human {
-//     // Public properties
-//     age = 13;
-//     name = 80;
-//     ht = 180;
+// Defining the class
+class Human {
+    // Public properties
+    Age = 13;
+    name = 80;
+    Height = 180;
 
-//     // Private property
-//     #wt = "hellow";
+    // Private property
+    #wt = "hellow";
 
-//     // Constructor
-//     constructor(newAge, newHeight) {
-//         this.age = newAge;
-//         this.ht = newHeight;
-//     }
+    //Parameter  Constructor
+    constructor(newAge, newHeight) {
+        this.age = newAge;
+        this.ht = newHeight;
+    }
 
-//     // Public method
-//     walking() {
-//         console.log("I am walking");
-//     }
+    // Public method
+    walking() {
+        console.log("I am walking");
+    }
 
-//     // Public method with fixed typo (was conslole.log)
-//     running() {
-//         console.log("I am running");
-//     }
+    // Public method with fixed typo (was conslole.log)
+    running() {
+        console.log("I am running");
+    }
 
-//     // Getter for private property
-//     get fetchName() {
-//         return this.#wt;
-//     }
+    // Getter for private property
+    get fetchName() {
+        return this.#wt;
+    }
 
-//     // Setter for private property
-//     set modifyName(val) {
-//         this.#wt = val;
-//     }
-// }
+    // Setter for private property
+    set modifyName(val) {
+        this.#wt = val;
+    }
+}
 
-// // Creating object with constructor values
-// let obj = new Human(40, 170);
+// Creating object with constructor values
+let obj = new Human(40, 170);
 
-// // Accessing public properties
-// console.log("Age:", obj.age);         // Output: 40
-// console.log("Height:", obj.ht);       // Output: 170
+// Accessing public properties
+console.log("Age:", obj.age);         // Output: 40
+console.log("Height:", obj.ht);       // Output: 170
 
-// // Attempting to access private field directly (will fail)
-// // console.log(obj.wt); // ❌ This will be undefined or throw error if strict
+// Attempting to access private field directly (will fail)
+// console.log(obj.wt); // ❌ This will be undefined or throw error if strict
 
-// // Correctly using getter for private field
-// console.log("Private value using getter:", obj.fetchName);
+// Correctly using getter for private field
+console.log("Private value using getter:", obj.fetchName);
 
-// // Modifying private value using setter
-// obj.modifyName = "updated value";
-// console.log("Updated private value:", obj.fetchName);
+// Modifying private value using setter
+obj.modifyName = "updated value";
+console.log("Updated private value:", obj.fetchName);
 
-// // Using class methods
-// obj.walking();
-// obj.running();
+// Using class methods
+obj.walking();
+obj.running();
 
-// // Logging the entire object (note: private fields are not shown)
-// console.log("Object:", obj);
+// Logging the entire object (note: private fields are not shown)
+console.log("Object:", obj);
 
 //Create the default functuion in the  js file 
 
 
-// function sayName(myName = "Babbar"){
-//     console.log("My name is ", myName);
-// }
-// sayName("hellow");
+function sayName(myName = "Babbar"){
+    console.log("My name is ", myName);
+}
+sayName("hellow");
 
 
- // //function is to run different type of the operation 
+ //function is to run different type of the operation 
 
-// function sayName(name = "Md Istekhar Alam "){
-//     console.log("my name is :" ,name);
-// }
-// sayName("love Babbar");
+function sayName(name = "Md Istekhar Alam "){
+    console.log("my name is :" ,name);
+}
+sayName("love Babbar");
 
 //create the multiple default parameter 
-// function MulName(lastName = "hellow jee", firstName){
-//     console.log( lastName," ", "Babbar");
-// }
-// MulName("Md Istekhar Alam");
+function MulName(lastName = "hellow jee", firstName){
+    console.log( lastName," ", "Babbar");
+}
+MulName("Md Istekhar Alam");
 
-//multiple default dependent parameter 
+//Multiple default dependent parameter 
 function solve (value = {age : 14, wt: 90, ht: 190}){
     console.log()
 }
+
+
+//use the properties of the maths
+console.log("Babbar");
+console.log(Math.PI);
+console.log(Math.min(4,6,90,456,72,));
+console.log(Math.round(2.5));
+console.log(Math.ceil(1.1));
+
+
+//Creation of the date 
+let curr = new Date();
+let date = new Date('june 21 may');
+let newDate = new Date('may 2025');
+console.log(newDate);
+
+
+
+
+
 
 
 
---
+++ Object.js
@@ -0,0 +1,12 @@
+let obj = {
+    age: 12,
+    wt:68,
+    ht:56,
+
+}
+console.log(obj);
+//change the color of the object in the run time 
+obj.col= "white ";
+console.log(obj);
+
+
