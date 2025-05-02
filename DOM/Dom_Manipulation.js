c784d2313d2e41b7d9719123f34ca5e1cd56f7a4
istekhar44 <67890istekharalam@gmail.com>
2025-05-02 00:33:48 -0700

    Add the dom file

---
+++ DOM/Dom_Manipulation.js
@@ -0,0 +1,7 @@
+// 🔍 What is the DOM?
+//Document Object Model
+// The DOM is a tree-like structure created by the browser to represent the HTML elements on a webpage. JavaScript can access and change this structure dynamically.
+// Add the html Tags and Remove the tags
+
+
+window.console.log("Love Babbar");
