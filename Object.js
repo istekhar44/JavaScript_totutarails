4c44f8a392bce814e0ad4ac2391053c77b2f7994
istekhar44 <67890istekharalam@gmail.com>
2025-05-02 00:11:23 -0700

    error handling

--- Object.js
+++ Object.js
@@ -1,12 +1,35 @@
-let obj = {
+let src = {
     age: 12,
     wt:68,
     ht:56,
 
 }
-console.log(obj);
-//change the color of the object in the run time 
-obj.col= "white ";
-console.log(obj);
+// console.log(obj);
+// //change the color of the object in the run time 
+// obj.col= "white ";
+// console.log(obj);
+     
+     // clone the sourse and destination of the object 
+// let dest = {...src};
+// src.age = 34;
+// console.log("SRC" ,src);
+// console.log("Dest",dest);
+
+
+let dest = {};
+for(let key in src){
+  let newKey = key ;
+  let newValue = src[key];
+
+  //insert newKey and the value in the dest 
+  dest[newKey] = newValue;
+}
+console.log ("SRC",dest);
+console.log ("dest",src);
+
+
+
+
+
 
 
---
+++ error_handling.js
@@ -0,0 +1,33 @@
+// Error Handling the  in the js
+// try {
+//     console.log("try block start ");
+//     //Referece block
+//     console.log(x);
+//     console.log("try block end here ");
+// }
+//       //Catch the error : X is not define
+// catch (error){
+//     console.log("I Am Inside catch ");
+//     console.log("Print the error", error);
+// }
+// finally {
+//     console.log("I will print the final  ");
+// }
+
+
+
+//      //coustom error
+// try {
+//       //Reference Error 
+//     console.log(x)
+// }
+// catch(error){
+//     throw new Error("Phala error declare kero ", "fir Print kero")
+// }
+// finally{
+//     console.log("there is no error ");
+// }
+
+
+
+
