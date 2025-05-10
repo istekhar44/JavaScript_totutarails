0ceeae20e8466429581f0f5ffe51baddd9c9d78b
istekhar44 <67890istekharalam@gmail.com>
2025-05-09 22:56:50 -0700

    add the new dom

--- DOM3/script.js
+++ DOM3/script.js
@@ -1,2 +1,22 @@
-console.log("hellow ");
-alert("apana collage");
+// console.log("hellow ");
+// alert("apana collage");
+// window.alert("hellow");
+// print the whole html code in the console window
+// console.dir(Document.body);
+// console.log(DOMPointReadOnlyocument.head);
+
+
+    //Access the htmal element by id
+ let heading = document.getElementById("heading1");   
+ console.log(heading);
+ console.dir(heading);
+
+
+       //Access the html element by class
+ let headings = document.getElementsByClassName("heading-class");
+ console.log(headings);
+ console.dir(heading);
+
+      //Access the html elemrnt by the 
+
+
